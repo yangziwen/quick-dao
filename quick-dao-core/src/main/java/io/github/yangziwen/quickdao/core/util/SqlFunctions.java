211532611a3654a503de83c2eae1de5c0034f996
yangziwen <yangziwen@meituan.com>
2020-04-19 12:08:41 +0800

    upgrade to 0.0.15

--- quick-dao-core/src/main/java/io/github/yangziwen/quickdao/core/util/SqlFunctions.java
+++ quick-dao-core/src/main/java/io/github/yangziwen/quickdao/core/util/SqlFunctions.java
@@ -4,21 +4,21 @@ public class SqlFunctions {
 
     public static final VarArgsSQLFunction DISTINCT_FUNC = new VarArgsSQLFunction("DISTINCT ", ", ", "");
 
-    public static final VarArgsSQLFunction COUNT_FUNC = new VarArgsSQLFunction("COUNT(", ",", ")");
+    public static final VarArgsSQLFunction COUNT_FUNC = new VarArgsSQLFunction("COUNT(", ", ", ")");
 
-    public static final VarArgsSQLFunction COUNT_DISTINCT_FUNC = new VarArgsSQLFunction("COUNT(DISTINCT ", ",", ")");
+    public static final VarArgsSQLFunction COUNT_DISTINCT_FUNC = new VarArgsSQLFunction("COUNT(DISTINCT ", ", ", ")");
 
     public static final VarArgsSQLFunction MYSQL_CONCAT_FUNC = new VarArgsSQLFunction("CONCAT(", ", ", ")");
 
     public static final VarArgsSQLFunction SQLITE_CONCAT_FUNC = new VarArgsSQLFunction("", "||", "");
 
-    public static final VarArgsSQLFunction MAX_FUNC = new VarArgsSQLFunction("MAX(", ",", ")");
+    public static final VarArgsSQLFunction MAX_FUNC = new VarArgsSQLFunction("MAX(", ", ", ")");
 
-    public static final VarArgsSQLFunction MIN_FUNC = new VarArgsSQLFunction("MIN(", ",", ")");
+    public static final VarArgsSQLFunction MIN_FUNC = new VarArgsSQLFunction("MIN(", ", ", ")");
 
-    public static final VarArgsSQLFunction AVG_FUNC = new VarArgsSQLFunction("AVG(", ",", ")");
+    public static final VarArgsSQLFunction AVG_FUNC = new VarArgsSQLFunction("AVG(", ", ", ")");
 
-    public static final VarArgsSQLFunction SUM_FUNC = new VarArgsSQLFunction("SUM(", ",", ")");
+    public static final VarArgsSQLFunction SUM_FUNC = new VarArgsSQLFunction("SUM(", ", ", ")");
 
     private SqlFunctions() {}
 
