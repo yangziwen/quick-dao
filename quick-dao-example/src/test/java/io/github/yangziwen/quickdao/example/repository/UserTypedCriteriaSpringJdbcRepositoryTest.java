319008be8ac63453fd833ee41c9108b13ab23332
yangziwen <yangziwen@meituan.com>
2020-05-10 11:38:42 +0800

    添加一个基于and和or进行条件分组的单测

--- quick-dao-example/src/test/java/io/github/yangziwen/quickdao/example/repository/UserTypedCriteriaSpringJdbcRepositoryTest.java
+++ quick-dao-example/src/test/java/io/github/yangziwen/quickdao/example/repository/UserTypedCriteriaSpringJdbcRepositoryTest.java
@@ -1,7 +1,15 @@
 package io.github.yangziwen.quickdao.example.repository;
 
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import io.github.yangziwen.quickdao.core.TypedCriteria;
+import io.github.yangziwen.quickdao.example.entity.User;
+import io.github.yangziwen.quickdao.example.enums.Gender;
 import io.github.yangziwen.quickdao.example.repository.base.BaseUserTypedCriteriaRepositoryTest;
 
 public class UserTypedCriteriaSpringJdbcRepositoryTest extends BaseUserTypedCriteriaRepositoryTest {
@@ -10,6 +18,26 @@ public class UserTypedCriteriaSpringJdbcRepositoryTest extends BaseUserTypedCrit
     protected UserSpringJdbcRepository createRepository() {
         JdbcTemplate template = new JdbcTemplate(dataSource);
         return new UserSpringJdbcRepository(template);
+    }
+
+    @Test
+    public void testGetUserListByComplicatedCriteria() {
+        TypedCriteria<User> criteria = new TypedCriteria<>(User.class)
+                .and(User::getUsername).startWith("user")
+                .and()
+                    .or(User::getGender).eq(Gender.MALE)
+                    .or(User::getGender).eq(Gender.FEMALE)
+                .end()
+                .and()
+                    .or(User::getCreateTime).le(new Date())
+                .end()
+                .or()
+                    .and(User::getId).eq(1L)
+                .end();
+        TypedCriteria<User> newCriteria = TypedCriteria.fromParamMap(User.class, criteria.toParamMap());
+        List<User> list = createRepository().list(criteria);
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(criteria.toParamMap().toString(), newCriteria.toParamMap().toString());
     }
 
 }
