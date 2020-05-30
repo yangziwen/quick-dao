ddd626e2bd3b596055039d3bd59258e357e1a07c
yangziwen <yangziwen@meituan.com>
2020-05-30 21:56:48 +0800

    添加主页

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    title: 'Quick-DAO',
+    title: 'QuickDAO 项目文档',
     description: '对Spring JDBC, MyBatis, sql2o等orm框架进行封装和抽象，用于快速实现增删改查功能',
     base: '/quick-dao/',
     themeConfig: {
         nav: [
-            { text: '首页', link: '/' },
             { text: '快速开始', link: '/start/index.html' },
-            { text: 'Github', link: 'https://github.com/yangziwen/quick-dao', target: '_blank' }
+            { text: 'GitHub', link: 'https://github.com/yangziwen/quick-dao', target: '_blank' }
         ],
         sidebar: [{
             title: '快速开始',
