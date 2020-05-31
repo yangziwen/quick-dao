75452c47e88bc5d58d13e3b24a3432caec4e69a7
yangziwen <yangziwen@meituan.com>
2020-05-31 11:20:30 +0800

    添加快速开始的文档

--- docs/docs/README.md
+++ docs/docs/README.md
@@ -11,4 +11,5 @@ features:
   details: 基于Java DSL灵活构建各种类型的单表查询SQL，支持聚合查询、条件嵌套、以及一些常用函数的调用。
 - title: 类型安全
   details: 可利用getter方法的lambda表达式声明SQL中的各种字段，通过泛型约束保证Java DSL的正确性。
+footer: Apache License 2.0 | Copyright © 2019-present Ziwen Yang
 ---
--- quick-dao-example/src/main/java/io/github/yangziwen/quickdao/example/repository/UserSpringJdbcRepository.java
+++ quick-dao-example/src/main/java/io/github/yangziwen/quickdao/example/repository/UserSpringJdbcRepository.java
@@ -1,12 +1,15 @@
 package io.github.yangziwen.quickdao.example.repository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import io.github.yangziwen.quickdao.core.Criteria;
+import io.github.yangziwen.quickdao.core.Order.Direction;
 import io.github.yangziwen.quickdao.core.Query;
 import io.github.yangziwen.quickdao.example.entity.User;
+import io.github.yangziwen.quickdao.example.enums.Gender;
 import io.github.yangziwen.quickdao.springjdbc.BaseSpringJdbcRepository;
 
 public class UserSpringJdbcRepository extends BaseSpringJdbcRepository<User> {
@@ -18,6 +21,18 @@ public class UserSpringJdbcRepository extends BaseSpringJdbcRepository<User> {
     public List<User> getUserListByUsernameStartWith(String prefix) {
         Criteria criteria = new Criteria().and("username").startWith(prefix);
         return list(new Query().where(criteria));
+    }
+
+    public List<String> listUsernameOfEldestMaleUser(int limit) {
+        Query query = newTypedQuery()
+                .select(User::getUsername)
+                .where(criteria -> criteria
+                        .and(User::getGender).eq(Gender.MALE))
+                .orderBy(User::getGender, Direction.DESC)
+                .limit(limit);
+        return list(query).stream()
+                .map(User::getUsername)
+                .collect(Collectors.toList());
     }
 
 }
