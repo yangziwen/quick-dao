ddf1c8c0d0b1c3cd3e9a487f17a69ec01cc6b603
yangziwen <yangziwen@meituan.com>
2023-10-03 21:29:49 +0800

    diff-check-test-commit-3

--- quick-dao-example/src/test/java/io/github/yangziwen/quickdao/example/repository/RoleTypedCriteriaSpringJdbcRepositoryTest.java
+++ quick-dao-example/src/test/java/io/github/yangziwen/quickdao/example/repository/RoleTypedCriteriaSpringJdbcRepositoryTest.java
@@ -1,12 +1,27 @@
 package io.github.yangziwen.quickdao.example.repository;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.github.yangziwen.quickdao.example.entity.Role;
 import io.github.yangziwen.quickdao.example.repository.base.BaseRoleTypedCriteriaRepositoryTest;
 
 public class RoleTypedCriteriaSpringJdbcRepositoryTest extends BaseRoleTypedCriteriaRepositoryTest {
 
+    private static final Long SECOND_ROLE_ID = 2L;
+
     @Override
     protected RoleSpringJdbcRepository createRepository() {
         return new RoleSpringJdbcRepository(dataSource);
+    }
+
+    /**
+     * 测试按用户名查询角色
+     */
+    @Test
+    public void testGetRoleByUsername() {
+        Role role = createRepository().getRoleByUsername("user2");
+        Assert.assertEquals(SECOND_ROLE_ID, role.getId());
     }
 
 }
