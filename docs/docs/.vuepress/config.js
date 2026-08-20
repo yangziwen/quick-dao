62bb854fc512826e219f7e7dbae0a2c0ae75c68d
yangziwen <yangziwen@meituan.com>
2026-08-20 12:44:40 +0800

    upgrade vuepress from 1.x to 2.x (2.0.0-rc.30)

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -1,33 +1,38 @@
-module.exports = {
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { webpackBundler } from '@vuepress/bundler-webpack'
+
+export default defineUserConfig({
+    base: '/quick-dao/',
+    lang: 'zh-CN',
     title: 'QuickDAO 项目文档',
     description: '对Spring JDBC, MyBatis, sql2o等orm框架进行封装和抽象，用于快速实现增删改查功能',
-    base: '/quick-dao/',
-    themeConfig: {
-        nav: [
+    bundler: webpackBundler(),
+    theme: defaultTheme({
+        navbar: [
             { text: '首页', link: '/' },
-            { text: '快速开始', link: '/start/index.html' },
-            { text: '使用手册', link: '/manual/index.html' },
+            { text: '快速开始', link: '/start/' },
+            { text: '使用手册', link: '/manual/' },
             { text: 'GitHub', link: 'https://github.com/yangziwen/quick-dao', target: '_blank' }
         ],
-        displayAllHeaders: true,
-        sidebar: [{
-            title: '快速开始',
-            path: '/start/',
-            collapsable: false
-        }, {
-            title: '使用手册',
-            path: '/manual/',
-            collapsable: false
-        }],
-        sidebarDepth: 2,
-        smoothScroll: false
-    },
+        sidebar: {
+            '/start/': [{
+                text: '快速开始',
+                link: '/start/',
+                collapsible: false
+            }],
+            '/manual/': [{
+                text: '使用手册',
+                link: '/manual/',
+                collapsible: false
+            }]
+        },
+        sidebarDepth: 2
+    }),
     plugins: [
-        [
-            '@vuepress/google-analytics',
-            {
-              'ga': 'UA-193733260-2'
-            }
-        ]
+        googleAnalyticsPlugin({
+            id: 'UA-193733260-2'
+        })
     ]
-}
+})
